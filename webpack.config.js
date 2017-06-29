0010d8a529b44c96ae5e06b4e3d8ca4c23511427
Ruben Oostinga <ruben.oostinga@gmail.com>
2017-06-29 11:39:38 +0200

    Use webpack custom cli args

--- webpack.config.js
+++ webpack.config.js
@@ -6,107 +6,107 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const target = process.env.npm_lifecycle_event;
+module.exports = ({ analyzeBundle, build }) => {
+  const minimize = analyzeBundle || build;
 
-const minimize = target === 'build' || target === 'bundle';
+  const devtool = minimize ? 'source-maps' : 'eval-source-maps';
 
-const devtool = minimize ? 'source-maps' : 'eval-source-maps';
-
-const plugins = [
-  new HtmlWebpackPlugin({
-    template: 'index.html',
-  }),
-  new CopyWebpackPlugin([
-    {
-      from: 'images/**',
-    },
-    {
-      from: 'manifest.json',
-    },
-    {
-      from: 'bower_components/webcomponentsjs/*.js',
-      flatten: true,
-    },
-    {
-      from: 'service-worker.js',
-    },
-  ]),
-];
-
-if (minimize) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-      },
-      output: {
-        comments: false, // Also removes licences
-      },
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
     }),
-  );
-}
+    new CopyWebpackPlugin([
+      {
+        from: 'images/**',
+      },
+      {
+        from: 'manifest.json',
+      },
+      {
+        from: 'bower_components/webcomponentsjs/*.js',
+        flatten: true,
+      },
+      {
+        from: 'service-worker.js',
+      },
+    ]),
+  ];
 
-if (target === 'bundle') {
-  plugins.push(new BundleAnalyzerPlugin());
-}
+  if (analyzeBundle) {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
 
-module.exports = {
-  entry: './src/my-app.html',
-  output: {
-    path: path.resolve(__dirname, './build'),
-    filename: '[name].[chunkhash:8].js',
-    chunkFilename: '[name].[chunkhash:8].js',
-  },
-  module: {
-    loaders: [
-      {
-        test: require.resolve(
-          './bower_components/polymer-redux/dist/polymer-redux.html',
-        ),
-        use: [{ loader: 'exports-loader', options: 'PolymerRedux' }],
-      },
-      {
-        test: /\.html$/,
-        exclude: require.resolve('./index.html'),
-        use: [
-          { loader: 'babel-loader' },
-          {
-            loader: 'wc-loader',
-            options: {
-              minify: minimize,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(ttf|eot|svg|woff(2)?)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash:8].[ext]',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        query: {
-          name: '[name].[ext]?[hash]',
+  if (minimize) {
+    plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+          warnings: false,
         },
-      },
-    ],
-  },
-  plugins,
-  devServer: {
-    // serve index.html in place of 404 responses to allow HTML5 history
-    historyApiFallback: true,
-  },
-  devtool,
+        output: {
+          comments: false, // Also removes licences
+        },
+      }),
+    );
+  }
+
+  return {
+    entry: './src/my-app.html',
+    output: {
+      path: path.resolve(__dirname, './build'),
+      filename: '[name].[chunkhash:8].js',
+      chunkFilename: '[name].[chunkhash:8].js',
+    },
+    module: {
+      loaders: [
+        {
+          test: require.resolve(
+            './bower_components/polymer-redux/dist/polymer-redux.html',
+          ),
+          use: [{ loader: 'exports-loader', options: 'PolymerRedux' }],
+        },
+        {
+          test: /\.html$/,
+          exclude: require.resolve('./index.html'),
+          use: [
+            { loader: 'babel-loader' },
+            {
+              loader: 'wc-loader',
+              options: {
+                minify: minimize,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(ttf|eot|svg|woff(2)?)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[hash:8].[ext]',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'file-loader',
+          query: {
+            name: '[name].[ext]?[hash]',
+          },
+        },
+      ],
+    },
+    plugins,
+    devServer: {
+      // serve index.html in place of 404 responses to allow HTML5 history
+      historyApiFallback: true,
+    },
+    devtool,
+  };
 };
