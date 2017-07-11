31151876f156d07ab289434b3bcf0749e7a06c3a
Léon Rodenburg <lrodenburg@xebia.com>
2017-07-11 14:57:34 +0200

    Installed Jest Put reducer in separate file Unit test the reducer

---
+++ src/friend-list/friend-list-reducer.spec.js
@@ -0,0 +1,23 @@
+import friendListReducer from './friend-list-reducer';
+
+const initialState = [];
+
+describe('friendListReducer', () => {
+  it('can add a friend', () => {
+    const newState = friendListReducer(initialState, {
+      type: 'ADD_FRIEND',
+      friend: 'Test'
+    });
+
+    expect(newState).toHaveLength(1);
+    expect(newState[0]).toEqual('Test');
+  });
+
+  it('can gracefully ignore unknown actions', () => {
+    const newState = friendListReducer(initialState, {
+      type: 'NONEXISTENT'
+    });
+
+    expect(newState).toEqual(initialState);
+  });
+});
--- src/friend-list/redux-behaviour.spec.js
+++
@@ -1,8 +0,0 @@
-import PolymerRedux from './redux-behaviour';
-
-describe('Store', () => {
-  it('should know how to add a friend', () => {
-    let currentState = PolymerRedux.getState();
-    expect(true).toBeTrue();
-  });
-});
