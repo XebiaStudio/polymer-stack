b58d8737c60d8569f757f610548c6039cf340f1b
Léon Rodenburg <lrodenburg@xebia.com>
2017-07-11 10:46:54 +0200

    Wrote first unit test

---
+++ src/friend-list/redux-behaviour.spec.js
@@ -0,0 +1,8 @@
+import PolymerRedux from './redux-behaviour';
+
+describe('Store', () => {
+  it('should know how to add a friend', () => {
+    let currentState = PolymerRedux.getState();
+    expect(true).toBeTrue();
+  });
+});
